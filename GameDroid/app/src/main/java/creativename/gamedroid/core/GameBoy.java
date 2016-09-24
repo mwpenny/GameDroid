87a91d5d9b00b12c7bf814e7c45f0d705e6b6cda
Matt Penny <mwpenny@users.noreply.github.com>
2016-09-24 16:21:22 -0400

    Add project skeleton

---
+++ GameDroid/app/src/main/java/creativename/gamedroid/core/GameBoy.java
@@ -0,0 +1,33 @@
+package creativename.gamedroid.core;
+
+/*
+     _n_________________
+    |_|_______________|_|
+    |  ,-------------.  |
+    | |  .---------.  | |
+    | |  |         |  | |
+    | |  | GAME    |  | |
+    | |  |   DROID |  | |
+    | |  |         |  | |
+    | |  `---------'  | |
+    | `---------------' |
+    |   _               |
+    | _| |_         ,-. |
+    ||_ O _|   ,-. "._,"|
+    |  |_|    "._,"   A |
+    |    _  _    B      |
+    |   // //           |
+    |  // //    \\\\\\  |
+    |  `  `      \\\\\\ ,
+    |________...______,"
+
+    CreativeName 2016:
+      * Matt Penny
+      * Alan Wu
+      * Hammad Asad
+      * Brendan Marko
+ */
+
+/* Entry point to the emulator core */
+public class GameBoy {
+}
