5d5b7fb9e8e83013bccd26bf821b22a073638bd7
Matt Penny <mwpenny@users.noreply.github.com>
2016-09-27 19:17:14 -0400

    Set up basic cartridge memory map

--- GameDroid/app/src/main/java/creativename/gamedroid/core/Cartridge.java
+++ GameDroid/app/src/main/java/creativename/gamedroid/core/Cartridge.java
@@ -17,14 +17,14 @@ public class Cartridge {
     private String manufacturer;
     private ColorMode colorMode;
     private String licensee;
-    private boolean supportsSGB;    // SGB = Super GameBoy?
-    private byte mbcType;          // Memory bank controller present
-    private byte romBankCount;      // Number of 32KB ROM banks present
-    private byte extRamSize;        // Number of KB of extra RAM in cartridge
+    private boolean supportsSGB;    // SGB = Super GameBoy
+    private byte mbcType;           // Memory bank controller present
+    private byte[] rom;             // Size depends on ROM header
+    private int romBank1Start;      // Where in the rom array does $4000 point?
+    private byte[] extRam;          // Not always allocated (depends on ROM header)
+    private int extRamStart;        // Where in the extRam array (if allocated) does $A000 point?
     private GameLocale locale;
     private byte gameVersion;
-
-    // TODO: probably best to store ROM and graphics data here too
 
     // Getters for public data
     public String getTitle() { return title; }
@@ -32,11 +32,31 @@ public class Cartridge {
     public ColorMode getColorMode() { return colorMode; }
     public String getLicensee() { return licensee; }
     public boolean isSupportsSGB() { return supportsSGB; }
-    public byte getCartType() { return cartType; }
-    public byte getRomBankCount() { return romBankCount; }
-    public byte getExtRamSize() { return extRamSize; }
+    public byte getCartType() { return mbcType; }
     public GameLocale getLocale() { return locale; }
-    public byte getGaneVersion() { return gameVersion; }
+    public byte getGameVersion() { return gameVersion; }
+
+    public byte read(int address) {
+        // ROM bank 0 is fixed, 1 is switchable
+        if (address < 0x4000)
+            return rom[address];
+        else if (address < 0x8000)
+            return rom[romBank1Start + (address - 0x4000)];
+
+        // Cartridge RAM may not be present
+        else if (extRam.length > 0 && address > 0x9FFF && address < 0xC000)
+            return extRam[extRamStart + (address - 0xA000)];
+
+        else {
+            // TODO: exceptions for invalid memory locations?
+            return 0;
+        }
+    }
+
+    public void write(short address, byte value) {
+        // TODO: MBC logic (where value "written" actually signifies which bank to switch to)
+        // TODO: cartridge RAM
+    }
 
     public Cartridge(String path) {
         // TODO: parse header fields and store in Cartridge instance
