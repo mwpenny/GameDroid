761aa6a387764529feaede30109b77e5672d0e7d
Matt Penny <matt@mattp.ca>
2016-11-08 14:20:34 -0500

    Refactor splash activity permission detection/prompt and update readme

--- GameDroid/app/src/main/java/creativename/gamedroid/ui/SplashActivity.java
+++ GameDroid/app/src/main/java/creativename/gamedroid/ui/SplashActivity.java
@@ -6,7 +6,7 @@ import creativename.gamedroid.R;
 import android.Manifest;
 import android.os.Bundle;
 import android.os.AsyncTask;
-import android.widget.Toast;
+import android.support.annotation.NonNull;
 import android.content.Intent;
 import android.os.Environment;
 import android.app.AlertDialog;
@@ -17,86 +17,16 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.content.ContextCompat;
 
-// Splash screen for performing app initialization and loading ROM metadata
-public class SplashActivity extends AppCompatActivity
-{
-
-    // Request Code for Callback
-    private int EXT_STORAGE_REQUEST = 100;
-
+/* Splash screen for performing app initialization and loading ROM metadata */
+public class SplashActivity extends AppCompatActivity {
     AlertDialog fsWarning;
     FindRomsTask findRomsTask;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Asks the User for permissions to access their file system for ROMs
-        userFilePermissions();
-
+        checkPermissions();
     }
-
-    // userFilePermissions()
-    // This function performs a check for access privileges to the file system to access ROMs here.
-    // If improper privileges are not given the app cannot access ROM files and will not function on Android 6.0 and above!
-    private void userFilePermissions()
-    {
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
-        {
-
-            // If User previously rejected the request this block executes...
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE))
-            {
-                System.out.println("User previously rejected request and this block is running...");
-            }
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXT_STORAGE_REQUEST);
-
-        }
-
-        else
-        {
-
-            if (createAppDirs())
-            {
-                findRomsTask = new FindRomsTask();
-                findRomsTask.execute();
-            }
-
-        }
-
-    } // end : userFilePermissions
-
-    @Override
-    public void onRequestPermissionsResult(int request, String[] permissions, int[] results)
-    {
-
-        if (request == EXT_STORAGE_REQUEST)
-        {
-
-            if (results.length > 0 && results[0] == PackageManager.PERMISSION_GRANTED)
-            {
-                // Toast.makeText(this, "Enjoy your GameDroid experience!", Toast.LENGTH_LONG).show();
-
-                if (createAppDirs())
-                {
-                    findRomsTask = new FindRomsTask();
-                    findRomsTask.execute();
-                }
-            }
-
-            else
-            {
-                Toast.makeText(this, "Please accept the Storage Request to allow GameDroid to access ROM files!", Toast.LENGTH_LONG).show();
-                this.finish();
-            }
-
-        }
-
-    } // end : onRequestPermissionsResult
 
     @Override
     protected void onDestroy() {
@@ -115,26 +45,47 @@ public class SplashActivity extends AppCompatActivity
         if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED) || (!f.exists() && !f.mkdirs())) {
             // Could not create application directories! Can't continue!
             fsWarning = new AlertDialog.Builder(this)
-                            .setTitle(getString(R.string.dialog_direrror_title))
-                            .setMessage(getString(R.string.dialog_direrror_message))
-                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(final DialogInterface dialog, final int which) {
-                                    finish();
-                                    dialog.dismiss();
-                                }
-                            })
-                            .setOnCancelListener(new DialogInterface.OnCancelListener() {
-                                @Override
-                                public void onCancel(DialogInterface dialog) {
-                                    finish();
-                                }
-                            })
-                            .setIconAttribute(android.R.attr.alertDialogIcon)
-                            .show();
+                    .setTitle(getString(R.string.dialog_direrror_title))
+                    .setMessage(getString(R.string.dialog_direrror_message))
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(final DialogInterface dialog, final int which) {
+                            finish();
+                            dialog.dismiss();
+                        }
+                    })
+                    .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            finish();
+                        }
+                    })
+                    .setIconAttribute(android.R.attr.alertDialogIcon)
+                    .show();
             return false;
         }
         return true;
+    }
+
+    private void gdinit() {
+        if (createAppDirs()) {
+            findRomsTask = new FindRomsTask();
+            findRomsTask.execute();
+        }
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+            // Prompt user for filesytem write permission if not present (needed for accessing ROMs)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        else
+            gdinit();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int request, @NonNull String[] permissions, @NonNull int[] results) {
+        if (results.length > 0)
+            gdinit();
     }
 
     /* Async task for loading ROM metadata from cache or disk */
